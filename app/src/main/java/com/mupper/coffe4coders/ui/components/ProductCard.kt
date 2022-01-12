3a31f9eb0ec596a3e97d0b2f10e68ad742632331
jesus-medina <jesus.daniel.medina.cruz@gmail.com>
2022-01-12 13:24:38 -0800

    Usando el componente Surface

--- app/src/main/java/com/mupper/coffe4coders/ui/components/ProductCard.kt
+++ app/src/main/java/com/mupper/coffe4coders/ui/components/ProductCard.kt
@@ -2,17 +2,20 @@ package com.mupper.coffe4coders.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mupper.coffe4coders.R
+import com.mupper.coffe4coders.ui.theme.PlatziBlue
 
 @Composable
 fun ProductCard() {
@@ -21,9 +24,36 @@ fun ProductCard() {
             .fillMaxWidth()
             .padding(16.dp)
             .clickable { }
-            .size(480.dp)
+            .size(480.dp),
+        elevation = 10.dp,
+        shape = MaterialTheme.shapes.small
     ) {
         Image(painterResource(R.drawable.co), contentDescription = null)
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            color = PlatziBlue.copy(alpha = 0.2f)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text("Café de Colombia")
+                Text("Café de origen de las montañas")
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    Row() {
+                        Image(painterResource(R.drawable.flagco), contentDescription = null)
+                        Text(
+                            "$ 35 USD",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.End
+                        )
+                    }
+                }
+            }
+        }
     }
 }
 
